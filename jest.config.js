1b5e5bd9e597e0f056396c5521796001f3056711
Mehrad Rousta <mehrad77@gmail.com>
2020-04-18 13:55:18 +0200

    test: move jest configs to one place

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  coverageDirectory: "coverage",
+  coverageReporters: ["json-summary", "text"],
   globals: {
     skipBabel: true,
   },
   preset: "ts-jest",
+  roots: ["<rootDir>packages"],
   setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
   testEnvironment: "node",
+  testMatch: ["**/__tests__/*.+(ts|tsx|js)", "**/*.test.+(ts|tsx|js)"],
   testPathIgnorePatterns: ["/node_modules/", "/output/", ".cache"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
 };
