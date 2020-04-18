d3818fc3936ffbad6bd122b9bc6d27097c076478
Mehrad Rousta <mehrad77@gmail.com>
2020-04-18 13:23:23 +0200

    fix(frontend): add husky commands to frontend

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -1,24 +1,24 @@
-import * as React from 'react'
-import styled from '@emotion/styled'
-import { transparentize } from 'polished'
-import { Link } from 'gatsby'
+import * as React from "react";
+import styled from "@emotion/styled";
+import { transparentize } from "polished";
+import { Link } from "gatsby";
 
-import { heights, dimensions, colors } from '../styles/variables'
-import Container from './Container'
+import { heights, dimensions, colors } from "../styles/variables";
+import Container from "./Container";
 
 const StyledHeader = styled.header`
   height: ${heights.header}px;
   padding: 0 ${dimensions.containerPadding}rem;
   background-color: ${colors.brand};
   color: ${transparentize(0.5, colors.white)};
-`
+`;
 
 const HeaderInner = styled(Container)`
   display: flex;
   flex-direction: row;
   align-items: center;
   height: 100%;
-`
+`;
 
 const HomepageLink = styled(Link)`
   color: ${colors.white};
@@ -29,10 +29,10 @@ const HomepageLink = styled(Link)`
   &:focus {
     text-decoration: none;
   }
-`
+`;
 
 interface HeaderProps {
-  title: string
+  title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ title }) => (
@@ -41,6 +41,6 @@ const Header: React.FC<HeaderProps> = ({ title }) => (
       <HomepageLink to="/">{title}</HomepageLink>
     </HeaderInner>
   </StyledHeader>
-)
+);
 
-export default Header
+export default Header;
